33904477096a1cec60023798de0e939ae4c37cfe
Beytullah KALAY <beytullah.kalay@outlook.com>
2023-01-09 06:52:43 +0300

    add: new font and how to play ui panel

--- Assets/Scenes/UIButtonActions/MainMenuUIButtonActions.cs
+++ Assets/Scenes/UIButtonActions/MainMenuUIButtonActions.cs
@@ -5,6 +5,8 @@ namespace Scenes.UIButtonActions
 {
     public class MainMenuUIButtonActions : MonoBehaviour
     {
+        [SerializeField] private GameObject buttons;
+        [SerializeField] private GameObject howToPlayPanel;
         public void LoadGame()
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -13,6 +15,18 @@ namespace Scenes.UIButtonActions
         public void QuitGame()
         {
             Application.Quit();
+        }
+
+        public void HowToPlay()
+        {
+            buttons.SetActive(false);
+            howToPlayPanel.SetActive(true);
+        }
+
+        public void ReturnToMenuFromHowToPlay()
+        {
+            buttons.SetActive(true);
+            howToPlayPanel.SetActive(false);
         }
     }
 }
